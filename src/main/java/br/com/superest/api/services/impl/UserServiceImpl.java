e5c6e5f3ac55342830607a9c1cdccb6977a25e4c
Lucas Uanderson <lucasuanderson159@live.com>
2023-02-04 19:41:25 -0300

    classe user service impl findById

---
+++ src/main/java/br/com/superest/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package br.com.superest.api.services.impl;
+
+import br.com.superest.api.domain.Usuario;
+import br.com.superest.api.repositories.UserRepository;
+import br.com.superest.api.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+
+@Service
+public class UserServiceImpl implements UserServices {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public Usuario findById(Integer id) {
+        Optional<Usuario> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
+
