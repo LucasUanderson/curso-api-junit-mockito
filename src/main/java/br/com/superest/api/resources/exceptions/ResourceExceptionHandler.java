9609ee8295c420257d8515f471259310212932ac
Lucas Uanderson <lucasuanderson159@live.com>
2023-02-05 16:17:44 -0300

    tratamento de excecao para objeto nao encontrado

---
+++ src/main/java/br/com/superest/api/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,22 @@
+package br.com.superest.api.resources.exceptions;
+
+import br.com.superest.api.services.exceptions.ObjectNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException ex, HttpServletRequest resquest){
+        StandardError error =
+                new StandardError(LocalDateTime.now(), HttpStatus.NOT_FOUND.value(), ex.getMessage(), resquest.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+    }
+
+}
--- src/main/java/br/com/superest/api/services/impl/UserServiceImpl.java
+++ src/main/java/br/com/superest/api/services/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package br.com.superest.api.services.impl;
 import br.com.superest.api.domain.Usuario;
 import br.com.superest.api.repositories.UserRepository;
 import br.com.superest.api.services.UserServices;
+import br.com.superest.api.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,7 +19,7 @@ public class UserServiceImpl implements UserServices {
     @Override
     public Usuario findById(Integer id) {
         Optional<Usuario> obj = repository.findById(id);
-        return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
 
