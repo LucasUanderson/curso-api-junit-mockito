bfe1772e5b58068b911f8c10a0b4d0398bb0b9dd
Lucas Uanderson <lucasuanderson159@live.com>
2023-02-13 10:11:38 -0300

    teste para classe user service metodo create com sucesso

--- src/test/java/br/com/superest/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/superest/api/services/impl/UserServiceImplTest.java
@@ -88,7 +88,16 @@ class UserServiceImplTest {
     }
 
     @Test
-    void create() {
+    void whenCreateThenReturnSuccess() {
+        Mockito.when(repository.save(Mockito.any())).thenReturn(user);
+
+        User response = service.create(userDto);
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
     }
 
     @Test
