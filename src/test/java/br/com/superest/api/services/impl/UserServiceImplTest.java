47ce62fdfacb5a12adfd717902bfdfbf7fbb01a8
Lucas Uanderson <lucasuanderson159@live.com>
2023-02-13 11:32:29 -0300

    Revert "Revert "teste para classe user service metodo update com excecao""
    This reverts commit b5a06dd1e02dd4379d36a89d861d31c66de371c1.

--- src/test/java/br/com/superest/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/superest/api/services/impl/UserServiceImplTest.java
@@ -116,7 +116,19 @@ class UserServiceImplTest {
 
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        Mockito.when(repository.save(Mockito.any())).thenReturn(user);
+
+        User response = service.update(userDto);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+
+
     }
 
     @Test
